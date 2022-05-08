cc12c804e421d24ae2b935312cda1aeea0c75f8f
Vsirotkin <vsirotkin15@gmail.com>
2022-05-08 10:47:23 +0300

    added pizzaupdate.js

--- src/pizzerias/pizzeriadetail.js
+++ src/pizzerias/pizzeriadetail.js
@@ -1,7 +1,19 @@
 import React, {Component} from 'react';
+import PizzaUpdate from './pizzeriaupdate'
 
 
 class PizzaDetail extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showComponent: false,
+        };
+        this.updatePizzerialDetails = this.updatePizzeriaDetails.bind();
+    }
+    updatePizzeriaDetails() {
+        this.setState({ showComponent: true });
+    }
+
     render() {
         const obj = this.props.pizzariaDetail;
 
@@ -9,10 +21,17 @@ class PizzaDetail extends Component {
             <div style={{ color: "yellow", border: "1px solid yellow" }}>
                 <h4 style={{ color: 'red' }}>{obj.pizzeria_name}</h4>
                 <h5>
-                    Address: {obj.street}, {obj.city}, {obj.state} 
+                    Street: {obj.street}, {obj.city}, {obj.state} 
                 </h5>
                 <h6>Phone: {obj.phone_number}</h6>
                 <p style={{ color: 'blueviolet' }}>{obj.description}</p>
+                <button 
+                    style={{ backgroundColor: 'white'}}
+                    onClick={() => this.updatePizzeriaDetails()}
+                >
+                    Update
+                </button>
+                {this.state.showComponent ? <PizzaUpdate pizzariaUpdate={obj} /> : null}
             </div>
         );
     }
