dad2b61657ad32852d305d27a27bd5bbf5395fef
Vsirotkin <vsirotkin15@gmail.com>
2022-05-08 12:12:35 +0300

    added update functional

--- src/pizzerias/pizzeriaform.js
+++ src/pizzerias/pizzeriaform.js
@@ -22,131 +22,132 @@ class PizzaForm extends Component{
     
     handleChange(event) {
         this.setState({[event.target.name]: event.target.value});
-        }
-        handleSubmit(event) {
-            alert('A name was submitted: ' + this.state.value);
-            event.preventDefault();
+    }
 
-            axios
-                .post('http://127.0.0.1:8000/create/' , {
-                    pizzeria_name:  this.state.pizzeria_name,
-                    street:         this.state.street,
-                    city:           this.state.city,
-                    state:          this.state.state,
-                    zip_code:       this.state.zip_code,
-                    website:        this.state.website,
-                    phone_number:   this.state.phone_number,
-                    description:    this.state.description,
-                    email:          this.state.email,
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.value);
+        event.preventDefault();
 
-                })
-                .then((response) =>{
-                    console.log(response);
-                })
-                .catch(function(error) {
-                    console.log(error);
-                })
-        }
-        render() {
-            const {
-                pizzeria_name,
-                street,
-                city,
-                state,
-                zip_code,
-                website,
-                phone_number,
-                description,
-                email,
-            } = this.state;
-            return (
-                <form onSubmit={this.handleSubmit}>
-                    <div>
-                        Pizzeria
-                        <input
-                            type='text'
-                            name='pizzeria_name'
-                            value={pizzeria_name}
-                            onChange={this.handleChange}
-                        />
-                    </div>
-                    <div>
-                        Street
-                        <input
-                            type="text"
-                            name="street"
-                            value={street}
-                            onChange={this.handleChange}
-                        />
-                    </div>
-                    <div>
-                        City
-                        <input 
-                            type="text"
-                            name="city"
-                            value={city}
-                            onChange={this.handleChange}
-                        />
-                    </div>
-                    <div>
-                        State
-                        <input 
-                            type="text"
-                            name="state"
-                            value={state}
-                            onChange={this.handleChange}
-                        />
-                    </div>
-                    <div>
-                        Zip_Code
-                        <input
-                            type="text"
-                            name="zip_code"
-                            value={zip_code}
-                            onChange={this.handleChange}
-                        />
-                    </div>
-                    <div>
-                        WebSite
-                        <input 
-                            type="text"
-                            name="website"
-                            value={website}
-                            onChange={this.handleChange}
-                        />
-                    </div>
-                    <div>
-                        Phone
-                        <input 
-                            type="text"
-                            name="phone_number"
-                            value={phone_number}
-                            onChange={this.handleChange}
-                        />
-                    </div>
-                    <div>
-                        Description
-                        <input 
-                            type="text"
-                            name="description"
-                            value={description}
-                            onChange={this.handleChange}
+        axios
+            .post('http://127.0.0.1:8000/create/' , {
+                pizzeria_name:  this.state.pizzeria_name,
+                street:         this.state.street,
+                city:           this.state.city,
+                state:          this.state.state,
+                zip_code:       this.state.zip_code,
+                website:        this.state.website,
+                phone_number:   this.state.phone_number,
+                description:    this.state.description,
+                email:          this.state.email,
 
-                        />
-                    </div>
-                    <div>
-                        Email
-                        <input 
-                            type="text"
-                            name="email"
-                            value={email}
-                            onChange={this.handleChange}
-                        />
-                    </div>
-                    <input style={{backgroundColor:'white'}} type='submit' value='Submit' />
-                </form>
-            );
-        }
+            })
+            .then((response) =>{
+                console.log(response);
+            })
+            .catch(function(error) {
+                console.log(error);
+            })
+    }
+    render() {
+        const {
+            pizzeria_name,
+            street,
+            city,
+            state,
+            zip_code,
+            website,
+            phone_number,
+            description,
+            email,
+        } = this.state;
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <div>
+                    Pizzeria
+                    <input
+                        type='text'
+                        name='pizzeria_name'
+                        value={pizzeria_name}
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <div>
+                    Street
+                    <input
+                        type="text"
+                        name="street"
+                        value={street}
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <div>
+                    City
+                    <input 
+                        type="text"
+                        name="city"
+                        value={city}
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <div>
+                    State
+                    <input 
+                        type="text"
+                        name="state"
+                        value={state}
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <div>
+                    Zip_Code
+                    <input
+                        type="text"
+                        name="zip_code"
+                        value={zip_code}
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <div>
+                    WebSite
+                    <input 
+                        type="text"
+                        name="website"
+                        value={website}
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <div>
+                    Phone
+                    <input 
+                        type="text"
+                        name="phone_number"
+                        value={phone_number}
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <div>
+                    Description
+                    <input 
+                        type="text"
+                        name="description"
+                        value={description}
+                        onChange={this.handleChange}
+
+                    />
+                </div>
+                <div>
+                    Email
+                    <input 
+                        type="text"
+                        name="email"
+                        value={email}
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <input style={{backgroundColor:'white'}} type='submit' value='Submit' />
+            </form>
+        );
+    }
 }
 
 
